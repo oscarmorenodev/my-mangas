ab0d039a321b34e86b701caafd80b5302f336003
Oscar Moreno <oscar@oscarmoreno.com>
2024-03-31 20:32:26 +0200

    MM-9 Mangas preview data added

---
+++ MyMangas/Preview Content/MangasPreview.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct PreviewData: DataInteractor {
+    let urlMangasPreview = Bundle.main.url(forResource: "mangasPreview", withExtension: "json")!
+    
+    func getMangas() async throws -> Mangas {
+        try loadPreviewData(url: urlMangasPreview)
+    }
+    
+    func loadPreviewData<T>(url: URL) throws -> T where T: Decodable {
+        let data = try Data(contentsOf: url)
+        return try JSONDecoder().decode(T.self, from: data)
+    }
+}
+
+extension MangasListViewModel {
+    static let preview = MangasListViewModel(interactor: PreviewData())
+}
+
+extension ContentView {
+    static var preview: some View {
+        let vm = MangasListViewModel.preview
+        
+        return ContentView()
+            .task {
+                _ = await vm.getMangas()
+            }
+            .environment(vm)
+    }
+}
