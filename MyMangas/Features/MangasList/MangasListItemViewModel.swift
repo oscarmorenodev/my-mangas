63343bfb1007abd8eb9ef4ec2703d5f37d6fb24f
Oscar Moreno <oscar@oscarmoreno.com>
2024-10-21 23:04:26 +0200

    MangasListItemViewModel refactored to match properties order

--- MyMangas/Features/MangasList/MangasListItemViewModel.swift
+++ MyMangas/Features/MangasList/MangasListItemViewModel.swift
@@ -9,9 +9,9 @@ struct MangasListItemViewModel: Identifiable, Hashable {
     init(manga: Manga) {
         id = manga.id
         title = manga.title ?? ""
+        authors = manga.authors?.map { "\($0.firstName) \($0.lastName)" } ?? []
         synopsis = manga.sypnosis ?? ""
         mainPicture = manga.mainPicture ?? ""
         isFavourite = false
-        authors = manga.authors?.map { "\($0.firstName) \($0.lastName)" } ?? []
     }
 }
