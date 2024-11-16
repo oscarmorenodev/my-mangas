f0ee2cb4b3a6efadc2228023ef0557ba4dc35525
Oscar Moreno <oscar@oscarmoreno.com>
2024-11-16 16:26:52 +0100

    Detailed view added to search tab

--- MyMangas/Features/MangasSearch/MangasSearchView.swift
+++ MyMangas/Features/MangasSearch/MangasSearchView.swift
@@ -2,14 +2,26 @@ import SwiftUI
 
 struct MangasSearchView: View {
     @Bindable var vm: MangasSearchViewModel
+    @State var selected: MangaItemViewModel?
     
     var body: some View {
         NavigationView {
             ScrollView {
                 ForEach(vm.searchResults) { manga in
                     MangaItemView(manga: manga)
+                        .onTapGesture {
+                            selected = manga
+                        }
                 }
             }
+            .opacity(selected == nil ? 1.0 : 0.0)
+            .overlay(
+                Group {
+                    if selected != nil {
+                        MangaDetailView(selected: $selected)
+                    }
+                }
+            )
         }
         .addCustomSearchBar(searchText: $vm.searchText, placeholder: "Search for a manga")
         .onChange(of: vm.searchText) {
