5325e0a034aa8ced1c149b54c7066602dddc2abe
Oscar Moreno <oscar@oscarmoreno.com>
2024-11-17 17:15:46 +0100

    Fake url for no cover mangas

--- MyMangas/Utils/Extensions/String+URLFormated.swift
+++ MyMangas/Utils/Extensions/String+URLFormated.swift
@@ -3,7 +3,7 @@ import Foundation
 extension String {
     func formatedToUrl() -> URL {
         if self.isEmpty {
-            return URL(string: "")!
+            return URL(string: "https://fakeurl.com")!
         } else {
             return URL(string: String(self.dropFirst().dropLast()))!
         }
