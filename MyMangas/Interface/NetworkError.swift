bcbe5ef7674a938749dcf0ca574d4229dde2c61a
Oscar Moreno <oscar@oscarmoreno.com>
2025-01-04 08:11:00 +0100

    Encode error added

--- MyMangas/Interface/NetworkError.swift
+++ MyMangas/Interface/NetworkError.swift
@@ -1,19 +1,22 @@
 enum NetworkError: Error, CustomStringConvertible {
-    case general(Error)
-    case status(Int)
     case decode(Error)
+    case encode(Error)
+    case general(Error)
     case noHTTP
+    case status(Int)
     
     public var description: String {
         switch self {
-        case .general(let error):
-            "ERROR: \(error.localizedDescription)"
-        case .status(let int):
-            "ERROR: Status \(int)"
-        case .noHTTP:
-            "ERROR: It's not HTTP response"
         case .decode(let error):
             "ERROR: Unable to decode JSON. More info: \(error)"
+        case .encode(let error):
+            "ERROR: Unable to encode JSON. More info: \(error)"
+        case .general(let error):
+            "ERROR: \(error.localizedDescription)"
+        case .noHTTP:
+            "ERROR: It's not HTTP response"
+        case .status(let int):
+            "ERROR: Status \(int)"
         }
     }
 }
