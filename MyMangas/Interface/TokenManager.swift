af2e09bfd7b62a25a012377a105905e9f0ac008b
Oscar Moreno <oscar@oscarmoreno.com>
2025-04-12 16:56:35 +0200

    Login real created users added

---
+++ MyMangas/Interface/TokenManager.swift
@@ -0,0 +1,67 @@
+import Foundation
+import Security
+
+class TokenManager {
+    private static let service = "com.mymanga.auth"
+    private static let account = "authToken"
+    
+    static func saveToken(_ token: String) throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: token.data(using: .utf8)!
+        ]
+        
+        // First try to delete any existing token
+        SecItemDelete(query as CFDictionary)
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            throw TokenError.saveFailed
+        }
+    }
+    
+    static func getToken() throws -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true
+        ]
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        
+        if status == errSecItemNotFound {
+            return nil
+        }
+        
+        guard status == errSecSuccess,
+              let data = result as? Data,
+              let token = String(data: data, encoding: .utf8) else {
+            throw TokenError.retrievalFailed
+        }
+        
+        return token
+    }
+    
+    static func deleteToken() throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else {
+            throw TokenError.deletionFailed
+        }
+    }
+}
+
+enum TokenError: Error {
+    case saveFailed
+    case retrievalFailed
+    case deletionFailed
+} 
