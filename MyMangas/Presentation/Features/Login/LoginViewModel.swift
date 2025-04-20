30ee5f64f514d832569e7e5527dbba9968de60ff
Oscar Moreno <oscar@oscarmoreno.com>
2025-04-20 13:20:05 +0200

    ref: login refactored using use case

--- MyMangas/Presentation/Features/Login/LoginViewModel.swift
+++ MyMangas/Presentation/Features/Login/LoginViewModel.swift
@@ -2,17 +2,18 @@ import Foundation
 
 @Observable
 final class LoginViewModel {
-    let interactor: DataInteractor
+    private let loginUseCase: LoginUseCase
     var displayError = false
     var errorMessage = ""
     
-    init(interactor: DataInteractor = DataService.shared) {
-        self.interactor = interactor
+    
+    init(loginUseCase: LoginUseCase = LoginUseCase()) {
+        self.loginUseCase = loginUseCase
     }
     
     func login(email: String, password: String) async -> Bool {
         do {
-            let _ = try await interactor.login(email: email, password: password)
+            let _ = try await loginUseCase.execute(email: email, password: password)
             
             NotificationCenter.default.post(name: .userLoggedIn, object: nil)
             
