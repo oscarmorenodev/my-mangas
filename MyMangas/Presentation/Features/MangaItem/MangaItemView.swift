02ec6c2bb9d21ab38480a2b4f7bc5cb0694c20a0
Oscar Moreno <oscar@oscarmoreno.com>
2025-05-04 08:56:53 +0200

    ref: Cover feature refactored

--- MyMangas/Presentation/Features/MangaItem/MangaItemView.swift
+++ MyMangas/Presentation/Features/MangaItem/MangaItemView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct MangaItemView: View {
+    @State var detailViewMode: Bool = false
     var manga: MangaItem
     
     var body: some View {
@@ -9,9 +10,10 @@ struct MangaItemView: View {
                 cover
                     .resizable()
                     .scaledToFill()
-                    .frame(width: 150, height: 230)                .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .frame(width: detailViewMode ? 250 : 150,
+                           height: detailViewMode ? 420 : 230)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
                     .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
-                    .frame(height: 250)
             } placeholder: {
                 Image(systemName: SystemImage.placeholder.rawValue)
                     .resizable()
@@ -25,7 +27,6 @@ struct MangaItemView: View {
                     }
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
-                    .frame(height: 250)
             }
             Text(manga.title)
                 .font(.callout)
